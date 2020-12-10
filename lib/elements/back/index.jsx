9b3b004b86be037de7d66d5e7f001ae8a0617116
JackRyan8 <58072511+JackRyan8@users.noreply.github.com>
2020-12-10 15:11:24 +0000

    Add back button (#10)
    * Add back button
    
    * Formatting and adjustments
    
    Indentations
    Use className over class
    Change story implementation so story matches docs
    
    * Changes to spacing

---
+++ lib/elements/back/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Back = ({href, text}) =>{
+   return <a href={href} className="link-back">{text}</a>  
+}   
+
+Back.propTypes = {
+    href: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired
+}
+
+export default Back;
