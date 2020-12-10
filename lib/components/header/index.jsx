efcb537a5a14961bb21333b65479fde8d92fc25b
Thomas Moore <tomo@uchuu.io>
2020-12-10 16:16:00 +0000

    Adds tests for header component

--- lib/components/header/index.jsx
+++ lib/components/header/index.jsx
@@ -5,17 +5,17 @@ import PropTypes from 'prop-types';
 import seax from '../../images/seax.png';
 
 const Header = ({title, navLinks}) => (
-    <header className='header-background'>
+    <header className='header-background' data-testid='header'>
         <div className='header'>
             <nav className='nav'>
                 <div className='logo-wrap'>
                     <a href='/' className='logo-text seax'>
-                        <img src={seax} width="36" height="30" alt='Essex County Council logo' style={{marginRight: '10px'}} />
+                        <img src={seax} width='36' height='30' alt='Essex County Council logo' style={{marginRight: '10px'}} />
                         {title}
                     </a>
                 </div>
                 {navLinks.length > 0 && (
-                    <ul className='nav-links'>
+                    <ul className='nav-links' data-testid='header-links'>
                         {navLinks.map(({href, text}) => (
                             <li key={text}><a href={href}>{text}</a></li>
                         ))}
