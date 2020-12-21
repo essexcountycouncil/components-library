e74647e220f0bf4a284d7c1473f2b296b71d5b2f
Thomas Moore <tomo@uchuu.io>
2020-12-21 15:47:44 +0000

    Use classNames for components

--- lib/components/browser-check/index.jsx
+++ lib/components/browser-check/index.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export const BrowserCheck = () => (
     <div
         id="browser-check"
-        class="browser-check-background"
+        className="browser-check-background"
         role="region"
         aria-label="browsercheck"
     >
-        <div class="browser-check">
-            <p class="browser-content">
+        <div className="browser-check">
+            <p className="browser-content">
                 You’re currently using an old browser.{" "}
                 <a href="/browsers" title="Learn about the latest browsers">
                     Find out how to update your browser
