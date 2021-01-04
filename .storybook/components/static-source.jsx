89e20da2847d3211cf4d6b7a76e1a46db51b6bb4
Thomas Moore <tomo@uchuu.io>
2021-01-04 14:35:12 +0000

    Bring formatting to StaticSource code blocks with prettier

--- .storybook/components/static-source.jsx
+++ .storybook/components/static-source.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOMServer from "react-dom/server";
+import prettier from "prettier/standalone";
+import parserHtml from "prettier/parser-html";
 import { Source } from "@storybook/addon-docs/blocks";
 
-const StaticSource = ({ code }) => (
-    <Source language="html" code={ReactDOMServer.renderToStaticMarkup(code)} />
-);
+const StaticSource = ({ code }) => {
+    const staticCode = ReactDOMServer.renderToStaticMarkup(code);
+    const formattedCode = prettier.format(staticCode, {
+        parser: "html",
+        plugins: [parserHtml],
+    });
+
+    return <Source language="html" code={formattedCode} />;
+};
 
 export default StaticSource;
