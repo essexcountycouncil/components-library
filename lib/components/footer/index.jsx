23565eaf5dfd309fa7e50d647f3538bd543ca99c
JackRyan8 <jack.ryan@essex.gov.uk>
2021-01-04 13:11:39 +0000

    Add ClassName
    Change class to className

--- lib/components/footer/index.jsx
+++ lib/components/footer/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export const Footer = () => (
-    <footer id="main-footer" class="footer-container">
-        <div class="footer-top">
-            <div class="footer">
-            <div class="nav">
-                <ul class="nav-links">
+    <footer id="main-footer" className="footer-container">
+        <div className="footer-top">
+            <div className="footer">
+            <div className="nav">
+                <ul className="nav-links">
                 <li><a href="https://www.essex.gov.uk/topic/about-essexgovuk">About essex.gov.uk</a></li><li>
                     <a href="https://www.essex.gov.uk/accessibility">Accessibility</a></li><li>
                     <a href="https://www.essex.gov.uk/cookies">Cookies</a></li><li>
@@ -13,21 +13,21 @@ export const Footer = () => (
                     <a href="https://www.essex.gov.uk/privacy-and-data-protection">Privacy and data protection</a></li><li>
                     <a href="/topic/contact-us">Contact us</a></li>
                 </ul>
-                <ul class="social">
+                <ul className="social">
                 <li>
-                    <a href="https://www.facebook.com/essexcountycouncil"><span class="fab fa-facebook-f"></span><span>Facebook</span></a></li><li>
-                    <a href="https://twitter.com/essex_cc"><span class="fab fa-twitter"></span><span>Twitter</span></a></li><li>
-                    <a href="http://www.youtube.com/user/EssexCountyCouncil"><span class="fab fa-youtube"></span><span>Youtube</span></a></li><li>
-                    <a href="https://www.flickr.com/photos/essexcc/"><span class="fab fa-flickr"></span><span>Flickr</span></a></li>
+                    <a href="https://www.facebook.com/essexcountycouncil"><span className="fab fa-facebook-f"></span><span>Facebook</span></a></li><li>
+                    <a href="https://twitter.com/essex_cc"><span className="fab fa-twitter"></span><span>Twitter</span></a></li><li>
+                    <a href="http://www.youtube.com/user/EssexCountyCouncil"><span className="fab fa-youtube"></span><span>Youtube</span></a></li><li>
+                    <a href="https://www.flickr.com/photos/essexcc/"><span className="fab fa-flickr"></span><span>Flickr</span></a></li>
                 </ul>
             </div>
             </div>
         </div>
-        <div class="footer-bottom">
-            <div class="footer">
-            <ul class="credits">
-                <li class="copyright">© Essex County Council 2020</li>
-                <li class="design-link"><a href="https://servicedesign.blog.essex.gov.uk/">Built by the Service Design Team</a></li>
+        <div className="footer-bottom">
+            <div className="footer">
+            <ul className="credits">
+                <li className="copyright">© Essex County Council 2020</li>
+                <li className="design-link"><a href="https://servicedesign.blog.essex.gov.uk/">Built by the Service Design Team</a></li>
             </ul>
             </div>
         </div>
