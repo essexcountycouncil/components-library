34fb65f50aff8f98867d9dd44d7a00165b1ecfd0
JackRyan8 <jack.ryan@essex.gov.uk>
2021-01-06 15:20:02 +0000

    Added impact component

---
+++ lib/components/impact/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Impact = ({ href, linktext, supportingtext }) => {
+    return (
+        <div className="impact">
+            <div className="hero campaign">
+                <div className="overlay"></div>
+                <div className="overlay-box">
+                    <div className="overlay-content">
+                        <div className="content-heading">
+                            <a href={href}>{linktext}</a>
+                        </div>
+                        <p>{supportingtext}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+Impact.propTypes = {
+    href: PropTypes.string.isRequired,
+    linktext: PropTypes.string.isRequired,
+    supportingtext: PropTypes.string.isRequired,
+}
+
+export default Impact;
