cb038d73ae9d583c631bc52a0b644e810cfaa4bf
JackRyan8 <jack.ryan@essex.gov.uk>
2021-01-14 16:55:46 +0000

    Improved Link box list
    Created single link box

---
+++ lib/components/link-box-list/link-box-list.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const LinkBoxListItem = ({ href, text, supportingText }) => {
+    return (
+        <li className="link-box">
+            <div className="link-content">
+                <a href={href} className="sub-section-heading">
+                    {text}
+                </a>
+            <   p>{supportingText}</p>
+            </div>
+        </li>
+    );
+};
+
+LinkBoxListItem.propTypes = {
+    href: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    supportingText: PropTypes.string.isRequired,
+};
+
+export const LinkBoxList = ({ children }) => {
+    return (
+        <ul className="link-boxes">
+            {children}
+        </ul>
+    )
+};
+
+LinkBoxList.propTypes = {
+    children: PropTypes.node,
+}
+
+export default LinkBoxList;
