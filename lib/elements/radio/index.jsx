6efa11a09274ff3e63409c37f12229d034d0fb80
Thomas Moore <tomo@uchuu.io>
2021-04-09 21:01:14 +0100

    Fix failing test for radio values

--- lib/elements/radio/index.jsx
+++ lib/elements/radio/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export const Radio = ({ name, text, value }) => (
-    <div className="multiple-choice" data-testid="radio">
+export const Radio = ({ name, text, value, ...props }) => (
+    <div className="multiple-choice" data-testid="radio" {...props}>
         <input id={value} name={name} type="radio" value={value} />
         <label htmlFor={value}>{text}</label>
     </div>
