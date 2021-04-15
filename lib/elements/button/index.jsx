ad794c1ff7cf659d89d818c167e7e6b69503300b
Thomas Moore <tomo@uchuu.io>
2021-04-15 15:35:36 +0100

    Ability to pass through tags to replace <a>

--- lib/elements/button/index.jsx
+++ lib/elements/button/index.jsx
@@ -2,35 +2,37 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export const Button = (props) => {
-    if (props.type === "submit") {
-        return (
-            <input type="submit" className="button" value={props.children} />
-        );
+    const { children, href, tag: Tag, type } = props;
+
+    if (type === "submit") {
+        return <input type="submit" className="button" value={children} />;
     }
 
-    if (props.type === "text") {
+    if (type === "text") {
         return (
             <button className="text" type="button">
-                {props.children}
+                {children}
             </button>
         );
     }
 
     return (
-        <a className="button button-start" href={props.href} role="button">
-            {props.children}
-        </a>
+        <Tag className="button button-start" href={href} role="button">
+            {children}
+        </Tag>
     );
 };
 
 Button.propTypes = {
     children: PropTypes.node.isRequired,
     href: PropTypes.string,
+    tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
     type: PropTypes.oneOf(["button", "text", "submit"]),
 };
 
 Button.defaultProps = {
     href: "",
+    tag: "a",
     type: "button",
 };
 
