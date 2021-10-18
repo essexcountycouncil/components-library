0cf80a23fe6e6fcc2c75179d59995139a31a7e1a
Thomas Moore <tomo@uchuu.io>
2021-10-18 10:44:00 +0100

    Adds new signup type

--- lib/components/signup/index.jsx
+++ lib/components/signup/index.jsx
@@ -1,48 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const SignupMarkup = (
-    <>
-        <h2 className="section-heading">Stay up to date</h2>
-        <p>Get the latest news and information about your council services</p>
-        <form action="/search" method="get" className="form">
-            <div className="group">
-                <div className="signup-input">
-                    <label htmlFor="email">
-                        Enter your email address
-                        <input
-                            type="text"
-                            id="email"
-                            name="email"
-                            autoComplete="off"
-                        />
-                    </label>
-                </div>
-                <div className="signup-button" id="submitYourEmailAddress">
-                    <button type="submit" className="button subscribe">
-                        Subscribe
-                    </button>
-                </div>
-            </div>
-        </form>
-    </>
-);
+import ButtonSignup from "./button-signup";
+import InputSignup from "./input-signup";
 
-export const Signup = ({ isBig }) => (
-    <section
-        className={isBig ? "signup-background" : "signup"}
-        aria-label="Subscribe to email"
-    >
-        {isBig ? <div className="signup">{SignupMarkup}</div> : SignupMarkup}
-    </section>
-);
+export const Signup = ({ href, isBig, input }) => {
+    if (input) return <InputSignup isBig={isBig} />;
+    return <ButtonSignup href={href} isBig={isBig} />;
+};
 
 Signup.propTypes = {
+    href: PropTypes.string,
     isBig: PropTypes.bool,
+    input: PropTypes.bool,
 };
 
 Signup.defaultProps = {
+    href: "",
     isBig: false,
+    input: false,
 };
 
 export default Signup;
